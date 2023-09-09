63d9e23369df9ade73cf88c452cfb6af0e4dedbd
Von-sledge <papaamoah568@gmail.com>
2023-09-09 05:32:46 +0000

    Variables, if, else, and while

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+/* Here contains the Headers */
+
+/* betty style doc for the main function in here */
+int main(void)
+{
+	int n;
+	$rand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/* Body of code */
+	return (0);
+}
