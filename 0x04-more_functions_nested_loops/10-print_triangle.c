d869a4b06476f9e2fa04248a88b1ecb0302a62a0
Von-sledge <papaamoah568@gmail.com>
2023-09-16 02:33:38 +0000

    More functions, more nested loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,13 +16,13 @@ void print_triangle(int size)
 	}
 	else
 	{
-		for (i = 1 ; i <= size ; i++)
+		for (i = 1; i <= size; i++)
 		{
-		for (j = 1 ; j <= i ; j++)
-		{
-		_putchar('#');
-		}
-	_putchar('\n');
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			 _putchar('\n');
 		}
 	}
 }
