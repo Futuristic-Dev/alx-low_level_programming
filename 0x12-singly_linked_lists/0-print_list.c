ac1080543011f5103df26b32919c3457c474cfc5
Von-sledge <papaamoah568@gmail.com>
2023-10-24 10:54:28 +0000

    Singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,21 +9,20 @@
 
 size_t print_list(const list_t *h)
 {
-    unsigned short int i;
+	unsigned short int i;
 
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-        {
-            printf("[0] (nil)\n");
-        }
-        else
-        {
-            printf("[%u] %s\n", h->len, h->str);
-        }
-        h = h->next;
-        i++;
-    }
-
-    return (i);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
