fd11975e720765fa3f5fc177c3fe2b232c5c9811
Von-sledge <papaaamoah568@gmail.com>
2023-12-23 06:14:01 +0000

    Hash Tables

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,12 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - implement a dbj2 algorithm
+ * copied source: @papamuziko/https://gist.github.com/papamuziko/
+ * 7bb52dfbb859fdffc4bd0f95b76f71e8
+ * @str: string used to generate hash value
+ * Return: hash value
+ */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+
+	return (hash);
 }
