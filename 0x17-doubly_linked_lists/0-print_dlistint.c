ed5ec154d007369663495a068694f52617b7cd8c
Von-sledge <papaaamoah568@gmail.com>
2024-01-02 00:30:52 +0000

    Doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,15 +6,14 @@
  * Return: The numnber of items of the list
  */
 
- size_t print_dlistint(const dlistint_t *h)
- {
-    size_t i;
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i;
 
-    for (i = 0; h; i++)
-    {
-        printf("%d\n",h->n);
-        h = h->next;
-    }
-    return (i);
- }
-
+	for (i = 0; h; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
