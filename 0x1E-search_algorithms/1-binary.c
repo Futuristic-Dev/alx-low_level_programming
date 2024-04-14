ce28064143643762d59be821ff35dddd0db03768
Futuristic-Dev <papaamoah568@gmail.com>
2024-04-14 23:37:03 +0000

    Add corrected version of Task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -18,31 +18,30 @@ int binary_search(int *array, size_t size, int value)
 	{
 		return (-1);
 	}
+
 	while (left <= right)
 	{
+		mid = (left + right) / 2;
+
 		printf("Searching in array: ");
 		for (i = left; i <= right; ++i)
 		{
-			printf("%d", array[i]);
-			if (i < right)
-				printf(", ");
-			else
-				printf("\n");
+			printf("%d%s", array[i], i == right ? "\n" : ", ");
 		}
 
-		mid = left + (right - left) / 2;
-
-		if (array[mid] == value)
-		{
-			return (mid);
-		}
 
 		if (array[mid] < value)
 		{
-			return (mid);
+			left = mid + 1;
+		}
+
+		else if (array[mid] > value)
+		{
+			right = mid - 1;
 		}
 		else
-			right = mid - 1;
+			return (mid);
+
 	}
 
 	return (-1);
